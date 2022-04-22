ea6256ba3dcf2d68113a944cfac311ab3ce3a6ff
Femar 01 <femar01@femar.local>
2022-04-22 11:45:57 -0300

    animacion cards flex

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
 import "./App.scss";
 
 const App = () => {
+  const sections = document.querySelectorAll(".section");
+
+  sections.forEach((section) => {
+    section.addEventListener("click", () => {
+      sections.forEach((section) => {
+        section.classList.remove("active");
+      });
+      section.classList.add("active");
+    });
+  });
+
   return (
-    <>
-      <nav>
-        <div className="container">
-          <h2>logo</h2>
-          <ul>
-            <li>Projects</li>
-            <li>About</li>
-            <li>Contact</li>
-          </ul>
-        </div>
-      </nav>
-    </>
+    <div>
+      <div className="container">
+        <section className="section one active">
+          <div className="card">
+            <h3>The Economist</h3>
+            Is greener, faster and more decentralized alternative to Bitcoin
+            possible?
+          </div>
+        </section>
+        <section className="section two ">
+          <div className="card">
+            <h3>The Economist</h3>
+            Is greener, faster and more decentralized alternative to Bitcoin
+            possible?
+          </div>
+        </section>
+        <section className="section three ">
+          <div className="card">
+            <h3>The Economist</h3>
+            Is greener, faster and more decentralized alternative to Bitcoin
+            possible?
+          </div>
+        </section>
+      </div>
+    </div>
   );
 };
 
